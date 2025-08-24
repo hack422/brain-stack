882c8cb847217c907654c35fa32b17e6aedd91d1
Pratham <prathamkhurana568@gmail.com>
2025-08-24 13:34:06 +0530

    sitemap updated

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,41 @@
+import { GetServerSideProps } from 'next';
+
+const Sitemap = () => {
+  return null;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://brainstackeducation.in';
+  
+  const pages = [
+    '',
+    '/materials',
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages
+    .map((page) => {
+      return `
+    <url>
+      <loc>${baseUrl}${page}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>${page === '' ? '1.0' : '0.8'}</priority>
+    </url>
+  `;
+    })
+    .join('')}
+</urlset>`;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Cache-Control', 's-maxage=30, stale-while-revalidate');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap; 
